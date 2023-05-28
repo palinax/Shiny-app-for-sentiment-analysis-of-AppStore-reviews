175063353204ad8ec8fecb946999284cfc8c48fd
Dominik Zabinski <dominikzabinski@gmail.com>
2023-05-28 13:28:07 +0200

    changing class representaiton

--- revapp/R/review.R
+++ revapp/R/review.R
@@ -1,10 +1,109 @@
-review <- setClass(Class = "review",
-                   representation = list(cont = "character"))
-setGeneric(name = "tokenize_review", def = function(x) standardGeneric(f = "tokenize_review"))
-setMethod(f = "tokenize_review",
+#' review Class
+#'
+#' A class representing a review.
+#'
+#' @name review
+#' @docType class
+#'
+#' @slot original The original text of the review.
+#' @slot processed The processed text of the review.
+#' @slot sent_score The sentiment score of the review.
+#'
+#' @examples
+#' review <- new("review", original = "This is a great product.")
+#' review
+#' @export
+setClass(Class = "review",
+         representation = list(original = "character",
+                               processed = "character",
+                               sent_score = "numeric"))
+
+review <- function(x) {
+    prc <- pre_process(x)
+    res <- new(Class = "review",
+               original = x,
+               processed = prc,
+               sent_score = sent_score(prc))
+    res
+}
+
+setMethod(f = "show",
           signature = "review",
-          definition = function(x)
-          {
-              unlist(strsplit(x = x@cont, split = "\\s"))
+          definition = function(x) {
+              message(paste0("original: ", x@original, "\n"))
+              message(paste0("processed: '", paste0(x@processed, collapse = "', '"), "'\n"))
           })
-tokenize_review(review(cont = "a"))
+
+setGeneric(name = "word_tokenize",
+           def = function(x) {
+               standardGeneric(f = "word_tokenize")
+           })
+
+setMethod(f = "word_tokenize",
+          signature = "character",
+          definition = function(x) {
+              unlist(strsplit(x = x, split = "\\s"))
+          })
+
+setGeneric(name = "remove_numerical_and_punct",
+           def = function(x) {
+               standardGeneric(f = "remove_numerical_and_punct")
+           })
+
+setMethod(f = "remove_numerical_and_punct",
+          signature = "character",
+          definition = function(x) {
+              gsub("[0-9[:punct:]]", " ", x)
+          })
+
+setGeneric(name = "remove_stop_words",
+           def = function(x) {
+               standardGeneric(f = "remove_stop_words")
+           })
+
+setMethod(f = "remove_stop_words",
+          signature = "character",
+          definition = function(x) {
+              stopwords <- c("the", "a", "i", "and")
+              setdiff(x, stopwords)
+          })
+
+setGeneric(name = "word_stem",
+           def = function(x) {
+               standardGeneric(f = "word_stem")
+           })
+
+setMethod(f = "word_stem",
+          signature = "character",
+          definition = function(x) {
+              substr(x, 1, 3)
+          })
+
+setGeneric(name = "pre_process",
+           def = function(x) {
+               standardGeneric(f = "pre_process")
+           })
+
+setMethod(f = "pre_process",
+          signature = "character",
+          definition = function(x) {
+              checkmate::assert_character(x)
+              x <- tolower(x)
+              x <- remove_numerical_and_punct(x)
+              x <- word_tokenize(x)
+              x <- remove_stop_words(x)
+              x <- word_stem(x)
+              return(x)
+          })
+
+setMethod(f = "pre_process",
+          signature = "review",
+          definition = function(x) {
+              pre_process(x@original)
+          })
+
+sent_score <- function(x) {
+    checkmate::assert_character(x)
+    set.seed(1234)
+    mean(runif(length(x), -1, 1))
+}
--- revapp/R/review_set.R
+++ revapp/R/review_set.R
@@ -1,10 +1,39 @@
-review_set <- setClass(Class = "review_set",
-                       representation = list(reviews = "list"))
-setGeneric(name = "tokenize_review_set", def = function(x) standardGeneric(f = "tokenize_review_set"))
-setMethod(f = "tokenize_review_set",
+#' review_set Class
+#'
+#' A class representing a set of reviews.
+#'
+#' @name review_set
+#' @docType class
+#'
+#' @slot reviews A list containing individual review objects.
+#' @slot words A data.table containing the words in the reviews.
+#' @slot sent_scores A numeric vector containing the sentiment scores of the reviews.
+#'
+#' @examples
+#' review1 <- new("review", original = "This is a great product.")
+#' review2 <- new("review", original = "I'm not satisfied with this product.")
+#'
+#' review_set <- new("review_set", reviews = list(review1, review2)
+#' review_set
+#' @export
+setClass(Class = "review_set",
+         representation = list(reviews = "list",
+                               words = "data.table",
+                               sent_scores = "numeric"))
+
+review_set <- function(x) {
+    words <- lapply(X = x, FUN = function(i) data.table(proc_word = i@processed))
+    words <- rbindlist(words)[,.(how_many = .N), by = "proc_word"]
+    new(Class = "review_set",
+        reviews = x,
+        words = words,
+        sent_scores = unlist(lapply(X = x, FUN = function(i) i@sent_score)))
+}
+
+setMethod(f = "show",
           signature = "review_set",
-          definition = function(x)
-          {
-              lapply(X = x@reviews, FUN = function(j) tokenize_review(j))
+          definition = function(x) {
+              message(paste0("Number of reviews: ", length(x@reviews), "\n"))
+              message(paste0("Most common words: '", paste0(x@words[order(-how_many)]$proc_word[1:3], collapse = "', '"), "'\n"))
+              message(paste0("Mean sentiment score (standard deviation): ", mean(x@sent_scores), " (", sd(x@sent_scores), ")\n"))
           })
-tokenize_review_set(review_set(reviews = list(review(cont = "a"), review(cont = "b"))))
