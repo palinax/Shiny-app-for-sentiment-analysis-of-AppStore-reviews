66b0ec32eb9fe021741a874826d88a4c2119f280
Dominik Zabinski <dominikzabinski@gmail.com>
2023-05-28 20:56:54 +0200

    functions for sentiment analysis

--- revapp/R/review_set.R
+++ revapp/R/review_set.R
@@ -17,9 +17,9 @@
 #' review_set
 #' @export
 setClass(Class = "review_set",
-         representation = list(reviews = "list",
-                               words = "data.table",
-                               sent_scores = "numeric"))
+         slots = list(reviews = "list",
+                      words = "data.table",
+                      sent_scores = "numeric"))
 
 #' Title
 #'
@@ -51,5 +51,67 @@ setMethod(f = "show",
           definition = function(x) {
               message(paste0("Number of reviews: ", length(x@reviews), "\n"))
               message(paste0("Most common words: '", paste0(x@words[order(-how_many)]$proc_word[1:3], collapse = "', '"), "'\n"))
-              message(paste0("Mean sentiment score (standard deviation): ", mean(x@sent_scores), " (", sd(x@sent_scores), ")\n"))
+              message(paste0("Mean sentiment score (standard deviation): ", mean(x@sent_scores, na.rm = T), " (", sd(x@sent_scores, na.rm = T), ")\n"))
           })
+
+#' Charts for set of reviews
+#'
+#' @param x object of class \code{review-set}
+#' @param br breaks for establish whether review is negative or positive
+#'
+#' @return charts
+#' @export
+#'
+#' @rdname review-set-charts
+#'
+#' @examples
+#' review1 <- review("This is a great product.")
+#' review2 <- review("I'm not satisfied with this product.")
+#'
+#' set_review <- review_set(list(review1, review2))
+#' sentimentDistributionPie(set_review)
+#' sentimentDistributionHist(set_review)
+sentimentDistributionPie <- function(x, br = c(-Inf, -0.1, 0.1, Inf)) {
+    res <- data.table(sent_score = x@sent_scores)
+    res[, lab := cut(x = sent_score, breaks = br, labels = c("negative", "neutral", "positive"))]
+    res[is.nan(sent_score), lab := "couldn't determine"]
+    res <- res[,.(l = .N), by = "lab"]
+    pie(res$l, labels = res$lab, main = "Sentiment distribution")
+}
+
+#' @name review-set-charts
+sentimentDistributionHist <- function(x) {
+    hist(x@sent_scores, main = "Sentiment distribution")
+}
+
+#' Title
+#'
+#' @param x
+#' @param br
+#'
+#' @return
+#' @export
+#'
+#' @examples
+sentimentRatio <- function(x, br = c(-Inf, 0, Inf)) {
+    res <- data.table(sent_score = x@sent_scores)
+    res[, lab := cut(x = sent_score, breaks = br, labels = c("negative", "neutral", "positive"))]
+    sum(res[lab == "positive"]) / sum(res[lab == "negative"])
+}
+
+#' Title
+#'
+#' @param x
+#'
+#' @return
+#' @export
+#'
+#' @examples
+sentimentSummary <- function(x) {
+    res <- na.omit(x@sent_scores)
+    list(mean = mean(res),
+         sd = sd(res),
+         min = min(res),
+         max = max(res),
+         dec = quantile(res, probs = 1:10 / 10))
+}
